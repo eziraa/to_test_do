dd0154551e69d03c7d263c9c8ab264c8e9a14c10
eziraa <ezratgab@gmail.com>
2025-03-17 17:24:34 +0300

    update:uodate todo selector silde method to select and reset todo

--- client/redux/slices/todo.slice.ts
+++ client/redux/slices/todo.slice.ts
@@ -1,24 +1,29 @@
-import { ITodoState } from "@/types/todo.type";
+import { todos } from "@/app/data/todos";
+import { ITodo, ITodoState } from "@/types/todo.type";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 
 
 const initialState: ITodoState = {
-  todos: [
-    { id: "mosisfie93293hedbsnnd234", title: "New Additions", description: "Stay representative of framework.", createdAt: "July 7, 2023" },
-    { id: "94384md8m84ycxbhsdffmfmd", title: "Bug Fixes", description: "Resolved layout issues in dark mode.", createdAt: "March 16, 2025" },
-    { id: "990394394sreuireretw5425", title: "Performance Update", description: "Optimized API requests.", createdAt: "March 10, 2025" },
-  ],
+  todos: todos,
   selectedTodo: null,
+  textToEdit: null,
 };
 
 const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    selectTodo: (state, action: PayloadAction<string>) => {
-      state.selectedTodo = state.todos.find(todo => todo.id === action.payload) || null;
+    selectTodo: (state, action: PayloadAction<ITodo | null>) => {
+      if(!action.payload) {
+        state.selectedTodo = null;
+        return;
+      }
+      state.selectedTodo = action.payload
     },
+    formatText: (state, action: PayloadAction<string | null>) => {
+      state.textToEdit = action.payload;
+    }
   },
 });
 
