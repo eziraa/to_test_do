cdfd5a4f5689b89f1ef14443efe0c67d5149d227
eziraa <ezratgab@gmail.com>
2025-03-17 13:03:23 +0300

    fix: server database connection issues

--- server/src/db.connection.ts
+++ server/src/db.connection.ts
@@ -1,17 +1,45 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/aurora_db';
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/test_todo';
 
-export async function connectToMongoDB() {
+// export async function initiateDbConnection() {
+//   try {
+//   console.log("MONGODB_URI",MONGODB_URI)
+//     await mongoose.connect(MONGODB_URI);
+//     console.log('Database Connection Successful...', MONGODB_URI);
+//   } catch (error) {
+//     console.error('Failed to connect to MongoDB:', error);
+//     process.exit(1);
+//   }
+// }
+
+import { MongoClient } from "mongodb";
+
+const username = encodeURIComponent("ezratgab");
+const password = encodeURIComponent("qQpfL2N6Yc2jc7Dm");
+const cluster = "cluster0";
+const authSource = "<authSource>";
+const authMechanism = "<authMechanism>";
+
+let uri = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/<dbName>?authSource=${authSource}&authMechanism=${authMechanism}`;
+
+const client = new MongoClient(uri);
+
+
+export default async function run() {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('Database Connection Successful...', MONGODB_URI);
-  } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
-    process.exit(1);
+    console.log("Connecting to the database", uri);
+    await client.connect().then(()=>{
+      console.log("Mongo Database Connected")
+    }).catch((err)=>{
+      console.error(err)
+    }
+    );
+  } finally {
+    await client.close();
   }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config';
-import { connectToMongoDB } from './db.connection';
+import  initiateDbConnection from './db.connection';
 import { createApp } from './app';
 
 const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   try {
-    await connectToMongoDB();
+    await initiateDbConnection().catch(console.dir);
+
     const app = createApp();
 
     app.listen(PORT, () => {
