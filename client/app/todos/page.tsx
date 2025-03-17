a1a63b2847a4270a75985da4eee0fdfb8772f2f4
eziraa <ezratgab@gmail.com>
2025-03-17 04:26:19 +0300

    update: todos page to add subheader

--- client/app/todos/page.tsx
+++ client/app/todos/page.tsx
@@ -1,13 +1,20 @@
+import { Header } from "./_components/Header";
 import Sidebar from "./_components/Sidebar";
+import SubHeader from "./_components/SubHeader";
 import TodoDetail from "./_components/ToDoDetail";
 
 export default function Home() {
   
   return (
-    <div className="flex h-screen bg-gray-200">
-      <Sidebar  />
-      <main className="flex-1 p-6">
-        <TodoDetail/>
+    <div className="flex text-slate-700 flex-col h-screen bg-gray-200">
+      <Header/>
+      <main className="flex px-14  w-screen p-6">
+        <Sidebar  />
+        <div className="flex w-full  flex-col p-4">
+          <SubHeader/>
+          <TodoDetail/>
+        </div>
+        
       </main>
     </div>
   );
